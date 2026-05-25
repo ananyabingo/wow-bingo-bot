d712fd564f454dd230463e25c98028e42e0613b1
ananyabingo <ananyaadefirs724@gmail.com>
2026-05-25 16:42:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-`API_TOKEN = '8718835672:AAFTPNHLrUgmbBlnTtx3DOCl3HQkdCm40Lg'`
+API_TOKEN = '8718835672:AAFTPNHLrUgmbBlnTtx3DOCl3HQkdCm40Lg'
 bot = telebot.TeleBot(API_TOKEN)
 
 WEBAPP_URL = "https://tiiny.site" 
