703429a76efb6bcc9b3f508831b8603f525333d6
ananyabingo <ananyaadefirs724@gmail.com>
2026-05-26 16:15:36 +0300

    Change API token in main.py
    Updated API token for the Telegram bot.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-API_TOKEN = '8718835672:AAE9L-d1aCzeWPNCQFkyaxadNE5-P1AQrow'
+API_TOKEN = '8718835672:AAFukM9HygVOX7TGrsOEO-8LGmcZTP0op3g'
 bot = telebot.TeleBot(API_TOKEN)
 
 WEBAPP_URL = "https://tiiny.site"
